a69373552bedf251433e31967c6c7fe3b30a0a8e
Vasyl Hrubyi <ronaldomen7@gmail.com>
2026-01-17 11:34:27 +0200

    add-console-log-in-connectMondo-db

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -4,6 +4,7 @@ export const connectMongoDB = async () => {
   try {
     const mongoUrl = process.env.MONGO_URL;
     await mongoose.connect(mongoUrl);
+    console.log("✅ MongoDB connection established successfully");
   } catch (error) {
     console.error(`Error conecting to MongoDB: ${error.message}`);
     process.exit(1);
